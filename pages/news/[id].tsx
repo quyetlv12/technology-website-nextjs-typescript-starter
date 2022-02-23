56d2cae58a8ea24ff544c6695dd708009d632d23
quyetfoxdevfront12 <lequyet840500@gmail.com>
2022-02-23 10:34:30 +0700

    update component

--- pages/news/[id].tsx
+++ pages/news/[id].tsx
@@ -1,8 +1,7 @@
 // You should use getStaticPaths if you’re statically pre-rendering pages that use dynamic routes
 import { GetStaticPaths } from "next";
 import { FC } from "react";
-import Comment from "../../components/molecules/website/comment/Comment";
-import BlogTemplate from "../../components/template/blogTemplate/BlogTemplate";
+import SideBar from "../../components/molecules/website/sidebar";
 import { convertISOstringToDate } from "../../configs";
 import { PostInterface } from "../../interfaces/post.interface";
 import PostService from "../../services/post.service";
@@ -11,67 +10,71 @@ interface Props {
 }
 const DetailPost: FC<Props> = ({ post }) => {
   return (
-    <BlogTemplate
-      children={
-        <main className="max-w-5xl mx-auto pb-10 pt-10">
-          <div className="flex flex-wrap overflow-hidden">
-            <div className="w-full">
-              <div className="mr-2 md:mr-4 ml-2">
-                <div className="pb-10">
-                  {/* first post */}
-                  <div>
-                    <img
-                      className="article-image"
-                      src="./images/1-770x518.jpg"
-                      alt=""
-                    />
-                    <h2 className="text-gray-900 font-serif text-3xl my-5 font-thin">
-                      <a>{post?.title}</a>
-                    </h2>
-                    <span className="text-xs text-gray-800 font-thin block mb-5">
-                      {post?.author}.{" "}
-                      {convertISOstringToDate(post?.createdDate)}
-                    </span>
-                    <div className="article-body">
-                      <p>{post?.content}</p>
+    <div>
+      <div className="flex flex-wrap -m-3">
+        <div className="md:w-1/4 w-full p-4">
+          <SideBar />
+        </div>
+        <div className="md:w-3/4 w-full">
+          <main className="max-w-5xl mx-auto pb-10 pt-10">
+            <div className="flex flex-wrap overflow-hidden">
+              <div className="w-full">
+                <div className="mr-2 md:mr-4 ml-2">
+                  <div className="pb-10">
+                    {/* first post */}
+                    <div>
+                      <img
+                        className="article-image"
+                        src="./images/1-770x518.jpg"
+                        alt=""
+                      />
+                      <h2 className="text-gray-900 font-serif text-3xl my-5 font-thin">
+                        <a>{post?.title}</a>
+                      </h2>
+                      <span className="text-xs text-gray-800 font-thin block mb-5">
+                        {post?.author}.{" "}
+                        {convertISOstringToDate(post?.createdDate)}
+                      </span>
+                      <div className="article-body">
+                        <p>{post?.content}</p>
+                      </div>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-          </div>
-          <Comment/>
-        </main>
-      }
-    />
+          </main>
+        </div>
+      </div>
+    </div>
   );
 };
-// export async function getStaticProps({ params }: any) {
-//   const { id } = params;
-//   const { data: post } = await PostService.getPost(id); // get info post
-//   return {
-//     props: {
-//       post: post,
-//     },
-//   };
-// }
+export async function getStaticProps({ params }: any) {
+  const { id } = params;
+  const { data: post } = await PostService.getPost(id); // get info post
+  return {
+    props: {
+      post: post,
+    },
+  };
+}
 
-// export const getStaticPaths: GetStaticPaths = async () => {
-//   const { data: posts } = await PostService.getPosts(); //fetch api get all products and return props
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { data: posts } = await PostService.getPosts(); //fetch api get all products and return props
 
-//   // create paths
-//   const paths = posts.map((post: any) => {
-//     return {
-//       params: {
-//         id: post.id,
-//       },
-//     };
-//   });
-//   // ==> return path to params
-//   return {
-//     paths,
-//     fallback: false,
-//   };
-// };
+  // create paths
+  const paths = posts.map((post: any) => {
+    return {
+      params: {
+        id: post.id,
+      },
+    };
+  });
+  // ==> return path to params
+  return {
+    paths,
+    fallback: false,
+  };
+};
 
 export default DetailPost;
--- pages/news/index.tsx
+++ pages/news/index.tsx
@@ -3,7 +3,6 @@ import { useForm } from "react-hook-form";
 import InputCustom from "../../components/atoms/input/InputCustom";
 import PostSection from "../../components/molecules/website/postSection/postSection";
 import SideBar from "../../components/molecules/website/sidebar";
-import BlogTemplate from "../../components/template/blogTemplate/BlogTemplate";
 import PostService from "../../services/post.service";
 export const getStaticProps = async () => {
   const res = await PostService.getPosts();
@@ -15,8 +14,22 @@ export const getStaticProps = async () => {
   };
 };
 const News = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const { register } = useForm();
   return (
-   <BlogTemplate children={<PostSection posts={posts}/>} />
+    <div>
+      <div className="flex flex-wrap -m-3">
+        <div className="md:w-1/4 w-full p-4">
+          <SideBar />
+        </div>
+        <div className="md:w-3/4 w-full">
+          <PostSection
+            title={"Danh sách bài viết"}
+            posts={posts}
+            maxWidth={2}
+          />
+        </div>
+      </div>
+    </div>
   );
 };
 
