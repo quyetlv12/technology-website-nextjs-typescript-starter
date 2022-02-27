053591bd44ccb7c7cf1a5d4ee6c1974eb532b4eb
quyetfoxdevfront12 <lequyet840500@gmail.com>
2022-02-27 22:37:35 +0700

    update login by google

--- apis/axiosClient.tsx
+++ apis/axiosClient.tsx
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const URL_API = 'http://localhost:4000/api'
+const URL_API = process.env.PRODUCTION ? "https://bloggerapi1.herokuapp.com/api" : 'http://localhost:4000/api'
 const httpClient = axios.create({
     baseURL: URL_API || process.env.URI,
     headers: {
