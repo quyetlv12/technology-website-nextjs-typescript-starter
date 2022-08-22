9752f47cd840052b1b9d4e5d0b78b40df8bfc4f4
quyetlv12 <lequyet840500@gmail.com>
2022-08-23 00:04:42 +0700

    add schedule

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -2,19 +2,9 @@ const Contact = () => {
   return (
     <section className="text-gray-600 body-font relative">
       <div className="container px-1 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
-        <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 p-10 flex items-end justify-start relative">
-          <iframe
-            width="100%"
-            height="100%"
-            className="absolute inset-0"
-            frameBorder={0}
-            title="map"
-            marginHeight={0}
-            marginWidth={0}
-            scrolling="no"
-            src="https://maps.google.com/maps?width=100%&height=600&hl=en&q=%C4%B0zmir+(My%20Business%20Name)&ie=UTF8&t=&z=14&iwloc=B&output=embed"
-            style={{ filter: "grayscale(1) contrast(1.2) opacity(0.4)" }}
-          />
+        <div className="lg:w-2/3 md:w-1/2 bg-gray-300 rounded-lg overflow-hidden sm:mr-10 flex items-end justify-start relative">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.3372785785687!2d105.83311581438853!3d21.059187192115967!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135aa541b811f21%3A0x8df3d1f6f5bd045c!2sVietlesson%20-%20Learn%20Languages%20%26%20Connect%20Cultures!5e0!3m2!1svi!2s!4v1661187622026!5m2!1svi!2s" className=" h-full" style={{border: 0 , width : "100%"}} loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
+
           <div className="bg-white relative flex flex-wrap py-6 rounded shadow-md">
             <div className="lg:w-1/2 px-6">
               <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs">
