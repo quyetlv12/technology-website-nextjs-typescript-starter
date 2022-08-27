afc0c346c403d918cccada8bcedd03d87b41e1a1
quyetlv12 <lequyet840500@gmail.com>
2022-08-27 17:59:48 +0700

    update homg page

--- components/molecules/website/contactFormBottom/index.tsx
+++ components/molecules/website/contactFormBottom/index.tsx
@@ -1,14 +1,15 @@
 import React, { FC } from 'react'
 import Title from '../../../atoms/title'
 interface Props {
-    name?: string
+    name?: string,
+    title?: String
 }
-const ContactFormBottom: FC<Props> = () => {
+const ContactFormBottom: FC<Props> = ({title}) => {
     return (
         <section className="text-gray-600 body-font relative">
-            <div className="container px-1 py-24 mx-auto">
+            <div className="container py-24 mx-auto">
                 <div className="flex flex-col text-center w-full mb-1">
-                    <Title title="Register for a course" titleVie='Đăng kí khoá học' />
+                    <Title title="Register for a course" titleVie={title} />
                 </div>
                 <div className="lg:w-1/2 md:w-2/3 mx-auto">
                     <div className="flex flex-wrap -m-2">
